79a63a857b94711a808fc348ae5dd3732e7d9694
Bryan Johnson Araujo <48476795+xKbryan@users.noreply.github.com>
2020-02-25 01:22:39 -0300

    Add files via upload

---
+++ assets/scripts/header.js
@@ -0,0 +1,19 @@
+document.getElementById("head").innerHTML = 
+`
+<link rel="stylesheet" type="text/css" media="screen" href="style.css">
+<link rel="stylesheet" href="../assets/materialize/css/materialize.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+`
+
+document.getElementById("header").innerHTML = 
+`
+<nav>
+  <div class="nav-wrapper orange accent-3">
+    <ul class="center-align">
+      <li><a href="filmes.html">Filmes</a></li>
+      <li><a href="series.html">Séries</a></li>
+      <li><a href="series.html">Sobre nós</a></li>
+    </ul>
+  </div>
+</nav>
+`
