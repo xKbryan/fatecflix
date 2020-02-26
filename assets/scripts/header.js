f8f2c464d77675fe981edfa232e2fa7eff64ad36
Bryan Johnson Araujo <xKbryan@users.noreply.github.com>
2020-02-26 17:03:54 -0300

    Alterado local para hospedagem no git

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,11 +1,11 @@
 document.getElementById("head").innerHTML = 
 `
-<link rel="stylesheet" type="text/css" media="screen" href="../assets/style.css">
-<link rel="stylesheet" href="../assets/materialize/css/materialize.min.css">
+<link rel="stylesheet" type="text/css" media="screen" href="assets/style.css">
+<link rel="stylesheet" href="assets/materialize/css/materialize.min.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="../assets/scripts/header.js"></script>
-<script src="../assets/materialize/js/materialize.js"></script>
+<script src="assets/scripts/header.js"></script>
+<script src="assets/materialize/js/materialize.js"></script>
 `
 
 document.getElementById("header").innerHTML = 
